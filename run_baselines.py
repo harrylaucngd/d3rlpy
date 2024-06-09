427fbff7436e55dd45aead065cfcb23ccc2f93d5
Jialin Zhao <zjl19970607@163.com>
2024-06-09 16:05:55 +0800

    tmp

--- run_baselines.py
+++ run_baselines.py
@@ -47,23 +47,26 @@ def run_experiment(env_name, algo_name):
     # print(f"best_bandwidth: {best_bandwidth}")
     # exit()
 
-    kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(all_states)
+    kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(all_states)
 
-    # Step 4: Calculate the density for a new state
-    # state = dataset.episodes[0].observations[0, :]  # Replace with your new state to estimate density
-    state = np.random.randn(17)
-    log_density = kde.score_samples(state.reshape(1, -1))
-    density = np.exp(log_density)
 
-    # Step 5: Compute pseudo-counts
-    pseudo_count = 1.0 / density
+    for i in range(10):
+        # Step 4: Calculate the density for a new state
+        state = dataset.episodes[0].observations[i, :]  # Replace with your new state to estimate density
+        print(f"state: {state}")
+        # state = np.random.randn(17)
+        log_density = kde.score_samples(state.reshape(1, -1))
+        density = np.exp(log_density)
 
-    # Step 6: Calculate exploration bonus
-    exploration_bonus = 1.0 / np.sqrt(pseudo_count)
+        # Step 5: Compute pseudo-counts
+        pseudo_count = density
 
-    print(f"Density: {density}")
-    print(f"Pseudo-Count: {pseudo_count}")
-    print(f"Exploration Bonus: {exploration_bonus}")
+        # Step 6: Calculate exploration bonus
+        exploration_bonus = 1.0 / np.sqrt(pseudo_count)
+
+        print(f"Density: {density}")
+        print(f"Pseudo-Count: {pseudo_count}")
+        print(f"Exploration Bonus: {exploration_bonus}")
     exit()
 
 
